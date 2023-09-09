02820fcf5689ef1d60d26320ade55d5333388fd3
Leonelzin <DouglasLuis2017@outlook.com.br>
2023-09-09 16:28:36 -0300

    new file:   .expo/README.md 	new file:   .expo/settings.json 	modified:   my-app/app/(tabs)/Home.tsx 	modified:   my-app/app/(tabs)/Login.tsx 	modified:   my-app/package-lock.json 	modified:   my-app/package.json

--- my-app/app/(tabs)/Home.tsx
+++ my-app/app/(tabs)/Home.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function HomeScreen() {
   const handleStartSleepRegulation = () => {
     // Implemente a lógica para iniciar a regulagem do sono aqui
     // Isso pode incluir a navegação para a tela de regulação do sono
+  };
+
+  const handleSetLocationManually = () => {
+    Alert.prompt(
+      'Definir Localização Manualmente',
+      'Insira a latitude e a longitude separadas por vírgula (ex: 12.345, -67.890)',
+      (text) => {
+        const [lat, long] = text.split(',').map((coord) => parseFloat(coord.trim()));
+
+        if (!isNaN(lat) && !isNaN(long)) {
+          // Você pode fazer algo com a latitude e longitude inseridas aqui, se necessário
+        } else {
+          Alert.alert('Erro', 'Por favor, insira valores de latitude e longitude válidos.');
+        }
+      }
+    );
   };
 
   return (
@@ -16,6 +33,12 @@ export default function HomeScreen() {
       </Text>
       <TouchableOpacity style={styles.getStartedButton} onPress={handleStartSleepRegulation}>
         <Text style={styles.getStartedButtonText}>Comece a Regular o Sono</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.manualLocationButton} onPress={handleSetLocationManually}>
+        <View style={styles.iconContainer}>
+          <Icon name="map-pin" size={20} color="#fff" />
+          <Text style={styles.manualLocationButtonText}>Definir Localização Manualmente</Text>
+        </View>
       </TouchableOpacity>
     </View>
   );
@@ -50,5 +73,23 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#fff',
     fontWeight: 'bold',
+  },
+  manualLocationButton: {
+    marginTop: 10,
+    backgroundColor: '#ff9900',
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 5,
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  manualLocationButtonText: {
+    textAlign: 'center',
+    color: '#fff',
+    fontWeight: 'bold',
+    marginLeft: 10,
   },
 });
--- my-app/app/(tabs)/Login.tsx
+++ my-app/app/(tabs)/Login.tsx
@@ -1,13 +1,78 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Platform } from 'react-native';
+import { LoginButton, AccessToken, LoginManager } from 'react-native-fbsdk-next';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
+let Biometrics: any = null;
+
+if (Platform.OS === 'android') {
+  Biometrics = require('react-native-fingerprint-scanner');
+} else if (Platform.OS === 'ios') {
+  Biometrics = require('react-native-touch-id');
+}
 
 export default function TabTwoScreen() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [biometryType, setBiometryType] = useState<string>('');
+
+  useEffect(() => {
+    checkBiometryType();
+  }, []);
+
+  const checkBiometryType = async () => {
+    try {
+      const biometryType = await Biometrics.isSupported();
+      setBiometryType(biometryType);
+    } catch (error) {
+      console.error('Erro ao verificar biometria:', error);
+    }
+  };
 
   const handleLogin = () => {
     // Implemente a lógica de autenticação aqui
-    // Você pode verificar o nome de usuário e senha e fazer a autenticação
+    // Você pode usar o valor de 'username' e 'password' para autenticar o usuário
+  };
+
+  const handleBiometricLogin = async () => {
+    try {
+      const result = await Biometrics.authenticate();
+
+      if (result) {
+        console.log('Autenticação biométrica bem-sucedida');
+        // Implemente a lógica para autenticar o usuário aqui.
+      } else {
+        console.log('Autenticação biométrica falhou ou foi cancelada');
+      }
+    } catch (error) {
+      console.error('Erro na autenticação biométrica:', error);
+    }
+  };
+
+  const handleFacebookLogin = async () => {
+    try {
+      const result = await LoginManager.logInWithPermissions(['public_profile', 'email']);
+      
+      if (result.isCancelled) {
+        console.log('Login do Facebook cancelado');
+      } else {
+        const data = await AccessToken.getCurrentAccessToken();
+        if (data) {
+          const accessToken = data.accessToken.toString();
+          console.log('Token de acesso do Facebook:', accessToken);
+        }
+      }
+    } catch (error) {
+      console.error('Erro no login do Facebook:', error);
+    }
+  };
+
+  const handleGoogleLogin = async () => {
+    try {
+      console.log('Login com o Google');
+    } catch (error) {
+      console.error('Erro no login com o Google:', error);
+    }
   };
 
   return (
@@ -28,6 +93,32 @@ export default function TabTwoScreen() {
       />
       <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
         <Text style={styles.loginButtonText}>Login</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handleFacebookLogin} style={styles.socialLoginContainer}>
+        <View style={styles.socialButton}>
+          <Icon name="facebook" size={24} color="#1877f2" style={styles.socialIcon} />
+          <Text style={styles.socialLoginText}>Login com Facebook</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handleGoogleLogin} style={styles.socialLoginContainer}>
+        <View style={styles.socialButton}>
+          <Icon name="google" size={24} color="#FF0000" style={styles.socialIcon} />
+          <Text style={styles.socialLoginText}>Login com Google</Text>
+        </View>
+      </TouchableOpacity>
+      {biometryType === 'FaceID' && (
+        <TouchableOpacity onPress={handleBiometricLogin} style={styles.socialLoginContainer}>
+          <View style={styles.socialButton}>
+            <Icon name="face" size={24} color="#007bff" style={styles.socialIcon} />
+            <Text style={styles.socialLoginText}>Login com Face ID</Text>
+          </View>
+        </TouchableOpacity>
+      )}
+      <TouchableOpacity onPress={handleBiometricLogin} style={styles.socialLoginContainer}>
+        <View style={styles.socialButton}>
+          <Icon name="camera" size={24} color="#007bff" style={styles.socialIcon} />
+          <Text style={styles.socialLoginText}>Login com a Face</Text>
+        </View>
       </TouchableOpacity>
       <TouchableOpacity>
         <Text style={styles.forgotPassword}>Esqueceu a senha?</Text>
@@ -74,9 +165,28 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontWeight: 'bold',
   },
+  socialLoginContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 5,
+  },
+  socialButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 8,
+    borderRadius: 5,
+  },
+  socialIcon: {
+    marginRight: 10,
+  },
+  socialLoginText: {
+    color: '#333',
+    fontWeight: 'bold',
+  },
   forgotPassword: {
     marginTop: 10,
     color: '#007bff',
+    fontWeight: 'bold',
   },
   signUpContainer: {
     flexDirection: 'row',
